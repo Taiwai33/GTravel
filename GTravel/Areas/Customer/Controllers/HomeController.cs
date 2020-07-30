7df449491102302b31b157c7d5e7524b9c077041
taiwai <tazgirl33@hotmail.com>
2020-07-30 10:49:13 +1000

    Home Controller updated

--- GTravel/Areas/Customer/Controllers/HomeController.cs
+++ GTravel/Areas/Customer/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using GTravel.Domain.ViewModels;
 using GTravel.Areas.Customer.Controllers;
 using GTravel.Domain.Data;
+using GTravel.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace GTravel.Controllers
 {
@@ -30,6 +32,20 @@ namespace GTravel.Controllers
             return View(homeIndexVM);
         }
 
+        public IActionResult Details(int tourId)
+        {
+            var dbTours = _db.Tours
+                .Include(p => p.TourCities)
+                    .ThenInclude(pc => pc.City)
+                .Include(p => p.TourCities)
+                    .ThenInclude(pc => pc.TourAttractions)
+                .Include(t=>t.Meal)
+                .FirstOrDefault(p => p.Id == tourId);
+
+            return View(dbTours);
+
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -39,6 +55,23 @@ namespace GTravel.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult StateDetails(string state)
+        {
+
+            ViewBag.state = state;
+            IEnumerable<Tour> tours = _db.Tours
+                .Include(p => p.TourCities)
+                    .ThenInclude(pc => pc.City)
+                    .Where(p => p.TourCities.Any(pc => pc.City.State == state))
+                    .ToList();
+
+
+
+            return View(tours);
+
+
         }
     }
 }
